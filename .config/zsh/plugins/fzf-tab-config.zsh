3d85802a7d5bc2d81d9e701e9744a4191a8277dd
Zile995 <stefan.zivkovic995@gmail.com>
2023-05-30 18:40:44 +0200

    zsh: fzf-tab: Add fzf preview for exa
    Signed-off-by: Zile995 <stefan.zivkovic995@gmail.com>

--- .config/zsh/plugins/fzf-tab-config.zsh
+++ .config/zsh/plugins/fzf-tab-config.zsh
@@ -1,8 +1,10 @@
-# Fzf-tab config
+# fzf-tab configuration
 zstyle ':fzf-tab:*' print-query alt-p
 zstyle ':fzf-tab:*' accept-line alt-enter
 zstyle ':fzf-tab:*' fzf-bindings 'enter:accept'
 zstyle ':fzf-tab:*' continuous-trigger 'alt-right'
+
+# fzf preview configuration
 zstyle ':fzf-tab:complete:tldr:argument-1' fzf-preview 'tldr --color $word'
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always --icons $realpath'
+zstyle ':fzf-tab:complete:(cd|exa):*' fzf-preview 'exa -1 --color=always --icons $realpath'
 zstyle ':fzf-tab:complete:systemctl-*:*' fzf-preview 'SYSTEMD_COLORS=1 systemctl status $word'
