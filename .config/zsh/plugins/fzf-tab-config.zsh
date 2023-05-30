be7393b74cc092b67108768b0481601ab8fb7c1c
Zile995 <stefan.zivkovic995@gmail.com>
2023-05-30 18:50:59 +0200

    zsh: fzf-tab: Add better systemctl preview
    Signed-off-by: Zile995 <stefan.zivkovic995@gmail.com>

--- .config/zsh/plugins/fzf-tab-config.zsh
+++ .config/zsh/plugins/fzf-tab-config.zsh
@@ -4,7 +4,7 @@ zstyle ':fzf-tab:*' accept-line alt-enter
 zstyle ':fzf-tab:*' fzf-bindings 'enter:accept'
 zstyle ':fzf-tab:*' continuous-trigger 'alt-right'
 
-# fzf preview configuration
+# fzf-tab preview configuration
 zstyle ':fzf-tab:complete:tldr:argument-1' fzf-preview 'tldr --color $word'
 zstyle ':fzf-tab:complete:(cd|exa):*' fzf-preview 'exa -1 --color=always --icons $realpath'
-zstyle ':fzf-tab:complete:systemctl-*:*' fzf-preview 'SYSTEMD_COLORS=1 systemctl status $word'
+zstyle ':fzf-tab:complete:systemctl-(status|(re|)start|(dis|en)able):*' fzf-preview 'SYSTEMD_COLORS=1 systemctl status $word'
