1023330787d6fb1ff2f56df5926765d015f50ec1
Zile995 <stefan.zivkovic995@gmail.com>
2023-06-06 23:48:34 +0200

    zsh: completion: Change completion header color
    Signed-off-by: Zile995 <stefan.zivkovic995@gmail.com>

--- .config/zsh/plugins/completion.zsh
+++ .config/zsh/plugins/completion.zsh
@@ -11,7 +11,8 @@ zstyle ':completion:*' special-dirs false
 zstyle ':completion:*:git-checkout:*' sort false
 zstyle ':completion:complete:*:options' sort false
 
-zstyle ':completion:*:descriptions' format '[%d]'
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*:descriptions' format "${fg_bold[green]}[%d]%{$reset_color%}"
+
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
 zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
