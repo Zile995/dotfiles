13a7d4f5a693473a249fa9676be698729dd7a613
Zile995 <stefan.zivkovic995@gmail.com>
2023-06-11 15:41:04 +0200

    zsh: colors: Use grc to colorize the output of some commands
    Signed-off-by: Zile995 <stefan.zivkovic995@gmail.com>

--- .config/zsh/plugins/colors.zsh
+++ .config/zsh/plugins/colors.zsh
@@ -8,3 +8,28 @@ source <(dircolors -b)
 alias ls='ls --color=tty'
 alias exa='exa --color=auto --icons'
 alias grep='grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox}'
+
+if [[ "$TERM" != dumb ]] && (( $+commands[grc] )) ; then
+  # Supported commands
+  cmds=(
+    as ant blkid cc configure curl cvs df diff dig dnf docker docker-compose \
+    docker-machine du env fdisk findmnt free g++ gas gcc getfacl getsebool gmake \
+    id ifconfig iostat ip iptables iwconfig kubectl last ldap lolcat ld lsattr lsblk \
+    lsmod lsof lspci make mount mtr mvn netstat nmap ntpdate php  ping ping6 proftpd ps \
+    sar semanage sensors showmount sockstat ss stat sysctl systemctl tcpdump traceroute \
+    traceroute6 tune2fs ulimit uptime vmstat wdiff whois \
+  );
+
+  # Set alias for available commands.
+  for cmd in $cmds ; do
+    if (( $+commands[$cmd] )) ; then
+      unalias $cmd &> /dev/null
+      $cmd() {
+        grc --colour=auto ${commands[$0]} "$@"
+      }
+    fi
+  done
+
+  # Clean up variables
+  unset cmds cmd
+fi
