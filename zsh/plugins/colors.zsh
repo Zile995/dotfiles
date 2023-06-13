12964a2a49d945fe7185e1ae94cd11b46eef2445
Zile995 <stefan.zivkovic995@gmail.com>
2023-06-13 16:08:39 +0200

    zsh: colors: Remove aliases
    Signed-off-by: Zile995 <stefan.zivkovic995@gmail.com>

--- zsh/plugins/colors.zsh
+++ zsh/plugins/colors.zsh
@@ -4,15 +4,9 @@ autoload -U colors && colors
 # Define LS_COLORS via dircolors
 source <(dircolors -b)
 
-# Set ls, grep, cat and exa colors
-alias ls='ls --color=tty'
-if (( $+commands[bat] )); then alias cat='bat -pp'; fi
-if (( $+commands[exa] )); then alias exa='exa --color=auto --icons'; fi
-alias grep='grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox}'
-
 () {
   local grc_plugin="/etc/grc.zsh"
-  if [ -f $grc_plugin ]; then
+  if [[ -e $grc_plugin ]]; then
     source "$grc_plugin" <$TTY
     unset -f ls
     unset -f journalctl
