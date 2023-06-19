1f49d5ba20c8e9c6bc95271e9e383c5308c63f1c
Zile995 <stefan.zivkovic995@gmail.com>
2023-06-19 23:13:56 +0200

    zsh: window-title: Add username and hostname for ssh connection
    Signed-off-by: Zile995 <stefan.zivkovic995@gmail.com>

--- zsh/plugins/window-title.zsh
+++ zsh/plugins/window-title.zsh
@@ -1,14 +1,32 @@
 autoload -Uz add-zsh-hook
 
-title-precmd () {
-  print -Pn -- '\e]2;%2~\a' >$TTY
+set-title() {
+  local title=$1
+  shift
+  print -Prnv title -- $title
+  printf -v title '\e]0;%s\a' "${(V)title}"
+  print -Pn -- $title >$TTY
 }
 
-title-preexec () {
-  print -Pn -- '\e]2;%2~ - ' && print -n -- "${(q)1}\a" >$TTY
+title-precmd() {
+  local title="%2~"
+  if [[ -n $SSH_CONNECTON || $P9K_SSH == 1 ]]; then
+    set-title "%n@%m: $title"
+  else
+    set-title "$title"
+  fi
+}
+
+title-preexec() {
+  local title="%2~"
+  if [[ -n $SSH_CONNECTON || $P9K_SSH == 1 ]]; then
+    set-title "%n@%m: $title - ${1//\%/%%}"
+  else
+    set-title "$title - ${1//\%/%%}"
+  fi
 }
 
 if [[ "$TERM" == (Eterm*|alacritty*|aterm*|foot*|gnome*|konsole*|kterm*|putty*|rxvt*|screen*|wezterm*|tmux*|xterm*) ]]; then
-  add-zsh-hook -Uz precmd title-precmd
+  add-zsh-hook -Uz precmd  title-precmd
   add-zsh-hook -Uz preexec title-preexec
 fi
