0decae5331b0bc3f0987073e49dbe4d08755d212
Zile995 <stefan.zivkovic995@gmail.com>
2023-06-20 14:37:44 +0200

    zsh: completion: Update completion
    Signed-off-by: Zile995 <stefan.zivkovic995@gmail.com>

--- zsh/plugins/completion.zsh
+++ zsh/plugins/completion.zsh
@@ -3,14 +3,36 @@ autoload -Uz compinit && compinit
 autoload -U +X bashcompinit && bashcompinit
 
 # Completion
-zstyle ':completion:*' menu select
-zstyle ':completion:*' use-cache on
-zstyle ':completion:*' group-name ''
-zstyle ':completion:*' special-dirs false
+zstyle ':completion:*'                  menu              "select"
+zstyle ':completion:*'                  verbose           "true"
+zstyle ':completion:*'                  group-name        ""
+zstyle ':completion:*'                  special-dirs      "false"
+zstyle ':completion:*'                  single-ignored    "show"
+zstyle ':completion:*'                  squeeze-slashes   "true"
+zstyle ':completion:*'                  use-cache         "true"
+zstyle ':completion:*'                  list-colors       "${(@s.:.)LS_COLORS}"
+zstyle ':completion:*'                  matcher-list      "m:{a-zA-Z}={A-Za-z}" "r:|=*" "l:|=* r:|=*"
+zstyle ':completion:::::'               insert-tab        "pending"
+zstyle ':completion:*:rm:*'             ignore-line       "other"
+zstyle ':completion:*:rm:*'             file-patterns     "*:all-files"
+zstyle ':completion:*:kill:*'           ignore-line       "other"
+zstyle ':completion:*:diff:*'           ignore-line       "other"
+zstyle ':completion:*:paths'            accept-exact-dirs "true"
+zstyle ':completion:*:functions'        ignored-patterns  "-*|_*"
+zstyle ':completion:*:-tilde-:*'        tag-order         "directory-stack" "named-directories" "users"
+zstyle ':completion:*:-subscript-:*'    tag-order         "indexes parameters"
+zstyle ':completion:*:*:*:*:processes'  command           "ps -u $USER -o pid,user,comm -w -w"
+zstyle ':completion:complete:*:options' sort              "false"
 
-zstyle ':completion:*:git-checkout:*' sort false
-zstyle ':completion:complete:*:options' sort false
+# Ssh
+zstyle ':completion:*:ssh:argument-1:*'                    sort             'true'
+zstyle ':completion:*:scp:argument-rest:*'                 sort             'true'
 
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
-zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
+# Git
+zstyle ':completion:*:git-checkout:*'                      sort             'false'
+zstyle ':completion:*:git-*:argument-rest:heads'           ignored-patterns '(FETCH_|ORIG_|*/|)HEAD'
+zstyle ':completion:*:git-*:argument-rest:heads-local'     ignored-patterns '(FETCH_|ORIG_|)HEAD'
+zstyle ':completion:*:git-*:argument-rest:heads-remote'    ignored-patterns '*/HEAD'
+zstyle ':completion:*:git-*:argument-rest:commits'         ignored-patterns '*'
+zstyle ':completion:*:git-*:argument-rest:commit-objects'  ignored-patterns '*'
+zstyle ':completion:*:git-*:argument-rest:recent-branches' ignored-patterns '*'
