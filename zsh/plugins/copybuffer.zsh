b0713c28d214ecdfbbbeddd010ff951efe24c17c
Zile995 <stefan.zivkovic995@gmail.com>
2023-06-26 21:29:21 +0200

    zsh: copybuffer: Simplify
    Signed-off-by: Zile995 <stefan.zivkovic995@gmail.com>

--- zsh/plugins/copybuffer.zsh
+++ zsh/plugins/copybuffer.zsh
@@ -1,9 +1,7 @@
 copybuffer() {
-  if (($+WAYLAND_DISPLAY && $+commands[wl-copy])); then
-    wl-copy -n <<< $BUFFER
-  elif (($+DISPLAY && $+commands[xclip])); then
-    xclip -r -in -sel c <<< $BUFFER
-  fi
+  (( $+commands[wl-copy] || $+commands[xclip] )) || return 1
+  (( $+WAYLAND_DISPLAY && $+commands[wl-copy] )) && wl-copy -n <<< $BUFFER
+  (( $+DISPLAY && $+commands[xclip] )) && xclip -r -in -sel c <<< $BUFFER
 }
 
 zle -N copybuffer
