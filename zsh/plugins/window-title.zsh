e58cddedc154b7814ca83ff05079d8b206b49738
Zile995 <stefan.zivkovic995@gmail.com>
2023-07-31 12:48:00 +0200

    zsh: window-title: Show the current working dir in title bar
    Signed-off-by: Zile995 <stefan.zivkovic995@gmail.com>

--- zsh/plugins/window-title.zsh
+++ zsh/plugins/window-title.zsh
@@ -1,10 +1,11 @@
 autoload -Uz add-zsh-hook
 
 set-title() {
+  emulate -L zsh -o no_prompt_bang -o prompt_subst -o prompt_percent -o no_prompt_subst
   local title=$1
   shift
   print -Prnv title -- $title
-  printf -v title '\e]0;%s\a' "${(V)title}"
+  printf -v title "\e]0;%s\a" "${(V)title}"
   print -Pn -- $title >$TTY
 }
 
@@ -15,14 +16,14 @@ title-precmd() {
   else
     set-title "$title"
   fi
+  printf "\e]7;file://%s%s\e\\" "$PWD" >$TTY
 }
 
 title-preexec() {
-  local title="%2~"
   if [[ -n $SSH_CONNECTON || $P9K_SSH == 1 ]]; then
-    set-title "%n@%m: $title - ${1//\%/%%}"
+    set-title "%n@%m: ${1//\%/%%}"
   else
-    set-title "$title - ${1//\%/%%}"
+    set-title "${1//\%/%%}"
   fi
 }
 
