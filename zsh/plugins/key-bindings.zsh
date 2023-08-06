8f79521f00ebef07cd5b19d159457fb3dff137a8
Zile995 <stefan.zivkovic995@gmail.com>
2023-08-06 10:53:30 +0200

    zsh: key-bindings: Add edit-command-line key-binding
    Signed-off-by: Zile995 <stefan.zivkovic995@gmail.com>

--- zsh/plugins/key-bindings.zsh
+++ zsh/plugins/key-bindings.zsh
@@ -25,6 +25,7 @@ key[Control-Backspace]="${terminfo[cub1]}"
     bindkey -M "$keymap" '^D'   exit-zsh
     bindkey -M "$keymap" '^[^R' restart-zsh
     bindkey -M "$keymap" '^[o'  xdg-open-dir
+    bindkey -M "$keymap" '^X^E' edit-command-line
     bindkey -M "$keymap" '^L'   clear-screen-and-scrollback
     [[ -n "${key[Home]}"              ]] && bindkey -M "$keymap" "${key[Home]}"              beginning-of-line
     [[ -n "${key[End]}"               ]] && bindkey -M "$keymap" "${key[End]}"               end-of-line
@@ -49,20 +50,15 @@ key[Control-Backspace]="${terminfo[cub1]}"
 # Finally, make sure the terminal is in application mode, when zle is active.
 if (( ${+terminfo[smkx]} && ${+terminfo[rmkx]} )); then
   autoload -Uz add-zle-hook-widget
-  function zle_application_mode_start { echoti smkx }
-  function zle_application_mode_stop { echoti rmkx }
+  zle_application_mode_start() { echoti smkx }
+  zle_application_mode_stop() { echoti rmkx }
   add-zle-hook-widget -Uz zle-line-init zle_application_mode_start
   add-zle-hook-widget -Uz zle-line-finish zle_application_mode_stop
 fi
 
-exit-zsh() { exit }
-
+exit-zsh()     { exit }
 xdg-open-dir() { xdg-open . }
-
-restart-zsh() {
-  zle -I
-  exec zsh <$TTY
-}
+restart-zsh()  { zle -I; exec zsh <$TTY }
 
 clear-screen-and-scrollback() {
   echoti civis >"$TTY"
@@ -73,13 +69,15 @@ clear-screen-and-scrollback() {
 }
 
 # Autoload widgets.
-autoload -U select-word-style && select-word-style bash	
+autoload -U edit-command-line
+autoload -U select-word-style && select-word-style bash
 autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
 
 # Define new widgets
 zle -N exit-zsh
 zle -N restart-zsh
 zle -N xdg-open-dir
+zle -N edit-command-line
 zle -N clear-screen-and-scrollback
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
