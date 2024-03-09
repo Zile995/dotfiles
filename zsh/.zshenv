61f2bf2e44231e8c9f508a74f860cfa91572025b
Zile995 <stefan.zivkovic995@gmail.com>
2024-03-09 22:20:23 +0100

    zsh: Update completions
    Signed-off-by: Zile995 <stefan.zivkovic995@gmail.com>

--- zsh/.zshenv
+++ zsh/.zshenv
@@ -1,6 +1,9 @@
 ZDOTDIR=${XDG_CONFIG_HOME:-$HOME/.config}/zsh
+ZCACHEDIR=${XDG_CACHE_HOME:-$HOME/.cache}/zsh
 ZPLUGINDIR=${ZDOTDIR}/plugins
 ZPROMPTDIR=${ZDOTDIR}/prompts
+ZFUNCTIONDIR=${ZDOTDIR}/functions
+ZCOMPLETIONDIR=${ZDOTDIR}/completions
 
 # Exports
 (( $+commands[nano] )) && export EDITOR=nano
@@ -16,17 +19,25 @@ export ANTIDOTE_HOME=${ZPLUGINDIR}/remote
   # Ensure fpath array does not contain duplicates
   typeset -gU fpath
 
+  # Create necessary dirs
+  [[ ! -d ZCACHEDIR ]]      && mkdir -pv "$ZCACHEDIR"
+  [[ ! -d ZPLUGINDIR ]]     && mkdir -pv "$ZPLUGINDIR"
+  [[ ! -d ZPROMPTDIR ]]     && mkdir -pv "$ZPROMPTDIR"
+  [[ ! -d ZFUNCTIONDIR ]]   && mkdir -pv "$ZFUNCTIONDIR"
+  [[ ! -d ZCOMPLETIONDIR ]] && mkdir -pv "$ZCOMPLETIONDIR"
+
+  # Add the antidote path
   local antidote_path=/usr/share/zsh-antidote
   [[ ! -r ${antidote_path}/antidote.zsh ]] && {
     antidote_path=${ZDOTDIR}/.antidote
-	[[ ! -r ${antidote_path}/antidote.zsh ]] && (( $+commands[git] )) &&
+    [[ ! -r ${antidote_path}/antidote.zsh ]] && (( $+commands[git] )) &&
       git clone --depth=1 https://github.com/mattmc3/antidote.git $antidote_path
   }
 
   # Add functions paths
   fpath=(
-    ${ZDOTDIR}/functions
-    ${ZDOTDIR}/completions
+    $ZFUNCTIONDIR
+    $ZCOMPLETIONDIR
     ${antidote_path}/functions
     $fpath
   )
--- zsh/plugins/local/completion.zsh
+++ zsh/plugins/local/completion.zsh
@@ -1,47 +1,77 @@
 () {
-  # Set the cache dir path
-  local cache_dir="${XDG_CACHE_HOME:-$HOME/.cache}/zsh"
-  [[ ! -d $cache_dir ]] && mkdir -p "$cache_dir"
+  local zcompcache=${ZCACHEDIR}/.zcompcache
+  local zcompdump=${ZCACHEDIR}/.zcompdump
 
   # Enable the completion cache and set the cache-path
   zstyle ':completion:*' use-cache  "true"
-  zstyle ':completion:*' cache-path "$cache_dir/.zcompcache"
+  zstyle ':completion:*' cache-path "$zcompcache"
 
   # Load zsh completion
-  autoload -Uz compinit && compinit -d "$cache_dir/.zcompdump"
+  autoload -Uz compinit && compinit -d "$zcompdump"
+
+  # Load bash completion
   autoload -U +X bashcompinit && bashcompinit
+
+  # zcompile completion files
+  zcompile_file "$zcompcache"
+  zcompile_file "$zcompdump"
 }
 
 # Completion
-zstyle ':completion:*'                  menu              "select"
-zstyle ':completion:*'                  verbose           "true"
-zstyle ':completion:*'                  group-name        ""
-zstyle ':completion:*'                  special-dirs      "false"
-zstyle ':completion:*'                  single-ignored    "show"
-zstyle ':completion:*'                  squeeze-slashes   "true"
+zstyle ':completion:*'                  menu              'select'
+zstyle ':completion:*'                  verbose           'true'
+zstyle ':completion:*'                  group-name        ''
+zstyle ':completion:*'                  special-dirs      'false'
+zstyle ':completion:*'                  single-ignored    'show'
+zstyle ':completion:*'                  squeeze-slashes   'true'
+zstyle ':completion:::::'               insert-tab        'pending'
 zstyle ':completion:*'                  list-colors       "${(@s.:.)LS_COLORS}"
-zstyle ':completion:*'                  matcher-list      "m:{a-zA-Z}={A-Za-z}" "r:|=*" "l:|=* r:|=*"
-zstyle ':completion:::::'               insert-tab        "pending"
-zstyle ':completion:*:rm:*'             ignore-line       "other"
-zstyle ':completion:*:rm:*'             file-patterns     "*:all-files"
-zstyle ':completion:*:kill:*'           ignore-line       "other"
-zstyle ':completion:*:diff:*'           ignore-line       "other"
-zstyle ':completion:*:paths'            accept-exact-dirs "true"
-zstyle ':completion:*:functions'        ignored-patterns  "-*|_*"
-zstyle ':completion:*:-tilde-:*'        tag-order         "directory-stack" "named-directories" "users"
-zstyle ':completion:*:-subscript-:*'    tag-order         "indexes parameters"
-zstyle ':completion:*:*:*:*:processes'  command           "ps -u $USER -o pid,user,comm -w -w"
-zstyle ':completion:complete:*:options' sort              "false"
+zstyle ':completion:*'                  matcher-list      'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*:options'          description       'yes'
+zstyle ':completion:*:options'          auto-description  '%d'
+zstyle ':completion:complete:*:options' sort              'false'
+zstyle ':completion:*:functions'        ignored-patterns  '-*|_*'
+zstyle ':completion:*:-subscript-:*'    tag-order         'indexes parameters'
 
-# Ssh
-zstyle ':completion:*:ssh:argument-1:*'                    sort             'true'
-zstyle ':completion:*:scp:argument-rest:*'                 sort             'true'
+# Directory
+zstyle ':completion:*:paths'     accept-exact-dirs  'true'
+zstyle ':completion:*:*:cd:*'    tag-order          local-directories directory-stack path-directories
+zstyle ':completion:*:-tilde-:*' group-order        'named-directories' 'path-directories' 'users' 'expand'
+
+# Ignored patterns
+zstyle ':completion:*:*:*:users' ignored-patterns \
+  adm amanda apache avahi beaglidx bin cacti canna clamav daemon \
+  dbus distcache dovecot fax ftp games gdm gkrellmd gopher \
+  hacluster haldaemon halt hsqldb ident junkbust ldap lp mail \
+  mailman mailnull mldonkey mysql nagios \
+  named netdump news nfsnobody nobody nscd ntp nut nx openvpn \
+  operator pcap postfix postgres privoxy pulse pvm quagga radvd \
+  rpc rpcuser rpm shutdown squid sshd sync uucp vcsa xfs '_*'
+
+# Ignore multiple entries.
+zstyle ':completion:*:rm:*'             file-patterns '*:all-files'
+zstyle ':completion:*:(rm|kill|diff):*' ignore-line   other
+
+# Kill
+zstyle ':completion:*:*:kill:*'         force-list  always
+zstyle ':completion:*:*:kill:*'         insert-ids  single
+zstyle ':completion:*:*:*:*:processes'  command     'ps -u $USER -o pid,user,comm -w -w'
 
 # Git
-zstyle ':completion:*:git-checkout:*'                      sort             'false'
-zstyle ':completion:*:git-*:argument-rest:heads'           ignored-patterns '(FETCH_|ORIG_|*/|)HEAD'
-zstyle ':completion:*:git-*:argument-rest:heads-local'     ignored-patterns '(FETCH_|ORIG_|)HEAD'
-zstyle ':completion:*:git-*:argument-rest:heads-remote'    ignored-patterns '*/HEAD'
-zstyle ':completion:*:git-*:argument-rest:commits'         ignored-patterns '*'
-zstyle ':completion:*:git-*:argument-rest:commit-objects'  ignored-patterns '*'
-zstyle ':completion:*:git-*:argument-rest:recent-branches' ignored-patterns '*'
+zstyle ':completion:*:git-checkout:*'                      sort              'false'
+zstyle ':completion:*:git-*:argument-rest:heads'           ignored-patterns  '(FETCH_|ORIG_|*/|)HEAD'
+zstyle ':completion:*:git-*:argument-rest:heads-local'     ignored-patterns  '(FETCH_|ORIG_|)HEAD'
+zstyle ':completion:*:git-*:argument-rest:heads-remote'    ignored-patterns  '*/HEAD'
+zstyle ':completion:*:git-*:argument-rest:commits'         ignored-patterns  '*'
+zstyle ':completion:*:git-*:argument-rest:commit-objects'  ignored-patterns  '*'
+zstyle ':completion:*:git-*:argument-rest:recent-branches' ignored-patterns  '*'
+
+# SSH/SCP/RSYNC
+zstyle ':completion:*:ssh:argument-1:*'                    sort              'true'
+zstyle ':completion:*:scp:argument-rest:*'                 sort              'true'
+zstyle ':completion:*:ssh:*'                               group-order       users hosts-domain hosts-host users hosts-ipaddr
+zstyle ':completion:*:(scp|rsync):*'                       group-order       users files all-files hosts-domain hosts-host hosts-ipaddr
+zstyle ':completion:*:(ssh|scp|rsync):*'                   tag-order         'hosts:-host:host hosts:-domain:domain hosts:-ipaddr:ip\ address *'
+zstyle ':completion:*:(ssh|scp|rsync):*:hosts-host'        ignored-patterns  '*(.|:)*' loopback ip6-loopback localhost ip6-localhost broadcasthost
+zstyle ':completion:*:(ssh|scp|rsync):*:hosts-domain'      ignored-patterns  '<->.<->.<->.<->' '^[-[:alnum:]]##(.[-[:alnum:]]##)##' '*@*'
+zstyle ':completion:*:(ssh|scp|rsync):*:hosts-ipaddr'      ignored-patterns  '^(<->.<->.<->.<->|(|::)([[:xdigit:].]##:(#c,2))##(|%*))' '127.0.0.<->' '255.255.255.255' '::1' 'fe80::*'
